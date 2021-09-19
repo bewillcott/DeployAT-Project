faac8ed5877a81e84312ed6531688cc7118a85be
Bradley Willcott <bw.opensource@yahoo.com>
2021-09-19 18:12:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Software Deployment Project
+This is a TAFE assignment for the Diploma in Software Development, at the South Metropolitan TAFE,
+Rockingham, Western Australia.
+
+The project entails developing a project plan for a DVD search application.
+
+As the program code was supplied as part of the assignment, there won't actually be any real code
+development.
---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * DeployATProject module description.
+ *
+ * @author <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+module DeployATProject {
+    requires mysql.connector.java;
+    requires java.sql;
+    requires java.desktop;
+    requires java.naming;
+}
